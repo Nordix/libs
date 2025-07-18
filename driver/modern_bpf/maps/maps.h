7d046f652bc9f79e593dc3bfdfae79e4df1f87e5
Tero Kauppinen <tero.kauppinen@est.tech>
2025-07-18 15:51:06 +0300

    Relocate enum ppm_syscall_support.
    Signed-off-by: Tero Kauppinen <tero.kauppinen@est.tech>

--- driver/modern_bpf/maps/maps.h
+++ driver/modern_bpf/maps/maps.h
@@ -122,15 +122,6 @@ struct {
 /*=============================== BPF_MAP_TYPE_ARRAY ===============================*/
 
 /**
- * @brief Control syscall filtering per direction.
- */
-enum ppm_syscall_support {
-	PPM_SC_SUPPORT_NONE = 0,
-	PPM_SC_SUPPORT_ENTER = 1 << 1,
-	PPM_SC_SUPPORT_EXIT = 1 << 2
-};
-
-/**
  * @brief This table is used to keep track of which syscalls must be filtered out
  * according to the simple consumer logic.
  */
